0ea2fd112ef4dc3456650586e168708aa04566e7
Archiatrus <daniel.koehntopp@gmail.com>
2018-02-06 20:14:05 +0100

    New ladder interface

--- src/main.cpp
+++ src/main.cpp
@@ -86,34 +86,7 @@ int main(int argc, char* argv[])
 #else
 #include "sc2lib/sc2_lib.h"
 #include "sc2utils/sc2_arg_parser.h"
-
-struct ConnectionOptions
-{
-	int32_t GamePort;
-	int32_t StartPort;
-	std::string ServerAddress;
-};
-
-bool ParseArguments(int argc, char *argv[], ConnectionOptions &connect_options)
-{
-	sc2::ArgParser arg_parser(argv[0]);
-	arg_parser.AddOptions({
-		{ "-g", "--GamePort", "Port of client to connect to", false },
-		{ "-o", "--StartPort", "Starting server port", false },
-		{ "-l", "--LadderServer", "Ladder server address", false },
-	});
-	arg_parser.Parse(argc, argv);
-	std::string GamePortStr;
-	if (arg_parser.Get("GamePort", GamePortStr)) {
-		connect_options.GamePort = atoi(GamePortStr.c_str());
-	}
-	std::string StartPortStr;
-	if (arg_parser.Get("StartPort", StartPortStr)) {
-		connect_options.StartPort = atoi(StartPortStr.c_str());
-	}
-	arg_parser.Get("LadderServer", connect_options.ServerAddress);
-	return true;
-}
+#include "LadderInterface.h"
 
 //*************************************************************************************************
 
@@ -161,25 +134,7 @@ int main(int argc, char* argv[]) {
 	}
 
 
-	// Add the custom bot, it will control the players.
-	CCBot bot;
-	coordinator.SetParticipants({
-		CreateParticipant(Util::GetRaceFromString(botRaceString), &bot),
-	});
-
-	// Start the game.
-
-	// Step forward the game simulation.
-	std::cout << "Connecting to port " << Options.GamePort << std::endl;
-	coordinator.Connect(Options.GamePort);
-	coordinator.SetupPorts(2, Options.StartPort, false);
-	// Step forward the game simulation.
-	coordinator.JoinGame();
-	coordinator.SetTimeoutMS(10000);
-	std::cout << " Successfully joined game" << std::endl;
-	while (coordinator.Update()) {
-	}
-
+	RunBot(argc, argv, new CCBot(), sc2::Race::Protoss);
 	return 0;
 }
 #endif // !LADDEREXE
